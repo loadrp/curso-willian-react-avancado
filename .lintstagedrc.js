9555aada50254574c7170e48ae2cd329296888b7
loadrp <lucas.rodrigues.prado120@gmail.com>
2024-01-07 15:20:13 -0300

    testando

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-    //Lint and pretiffy js and ts files
-    '*.{js,jsx,ts,tsx}': (filenames) => [
-        `prettier --write ${filenames.join(' ')}`
-        `npm run lint --fix . ${filenames.join(' --file')}`,
-    ]
+  //Lint and pretiffy js and ts files
+  '*.{js,jsx,ts,tsx}': (filenames) => [
+    `prettier --write ${filenames.join(' ')}`,
+    `npm run lint --fix . ${filenames.join(' --file')}`
+  ]
 }
